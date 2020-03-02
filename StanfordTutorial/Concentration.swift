97a8e1441411f335ec2ae2241001d461dfe0f905
JoseQ <josequintana@kelsus.com>
2020-03-02 11:41:38 -0300

    Basic game engine

--- StanfordTutorial/Concentration.swift
+++ StanfordTutorial/Concentration.swift
@@ -11,8 +11,28 @@ class Concentration {
     
     var cards = [Card]()
     
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
     func chooseCard(at index: Int) {
-        
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                //check if card match
+                if (cards[matchIndex].identifier == cards[index].identifier) {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            } else {
+                //either no cards or 2 cards faceup
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
     }
     
     init(numberOfPairsOfCards: Int) {
@@ -22,5 +42,9 @@ class Concentration {
         }
         
         cards.shuffle()
+    }
+    
+    func newGame() {
+        cards
     }
 }
